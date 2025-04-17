5bc780829689fe2ea919d813b399cb3eb1b4530e
Maksym Atamanchuk <maksumatmn@gmail.com>
2025-04-17 20:09:17 +0300

    dddS

--- src/sections/Perdun.jsx
+++ src/sections/Perdun.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Perdun() {
     return (
         <div className="mt-20">
-            <h1 className='text-5xl'>Facebook для старих пердунів, пішов ти нахуй</h1>
+            <h1 className='text-5xl'>Facebook для старих пердунів, пішов ти нахуй ЛОХ ЄБАНИЙ</h1>
         </div>
     )
 }
